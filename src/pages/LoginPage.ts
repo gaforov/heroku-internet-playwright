e8576c1af8f97139994933442f008bc725b46c58
Said Gaforov <sgaforov@gmail.com>
2025-02-06 13:34:15 -0800

    adding/optimizing LoginPage class

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -1,11 +1,6 @@
 import { BasePage } from './BasePage';
 
 export class LoginPage extends BasePage {
-    //  Direct Selectors: Simple, Single Interactions: Works well for straightforward cases where the selector is used once.
-    // private usernameInput = '#username';
-    // private passwordInput = '#password';
-    // private loginButton = 'button[type="submit"]';
-    // private successMessage = '.flash.success';
     private usernameInput = this.page.locator('#username');
     private passwordInput = this.page.locator('#password');
     private loginButton = this.page.locator('button[type="submit"]');
